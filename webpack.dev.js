1e8ffc311f6a57d5b92ae6b6df38d8edb5b51508
lalalic <lalalic@139.com>
2019-05-15 17:56:10 +0800

    3.4.20 we-edit

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,6 +15,16 @@ module.exports=(base,HTML,port=require("./package.json").config.devPort)=>{
 			host:"0.0.0.0",
 			before(app){
 				app.get("/app.apk.version",(req, res)=>res.json(require("./package.json").version))
+
+				app.get("/font-service.js", (req,res)=>{
+					res.set({ 'Content-Type': 'application/javascript; charset=utf-8' });
+                	res.send(require("fs").readFileSync(path.join(__dirname, 'node_modules/we-edit/font-service.js')));
+				})
+
+				app.get("/fonts/Arial", (req,res)=>{
+					res.set({ 'Content-Type': 'font' });
+                	res.send(require("fs").readFileSync(path.join(__dirname, 'node_modules/we-edit/Arial')));
+				})
 			}
 		},
 		plugins:[
