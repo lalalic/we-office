490892ca9d82ec0f894f6eaa0db6d6be77bbdc16
lalalic <lalalic@139.com>
2019-10-11 17:03:26 +0800

    cloud code build

--- webpack.cloud.js
+++ webpack.cloud.js
@@ -18,15 +18,7 @@ module.exports=()=>({
         filename:"__generated.js",
     },
     mode:"production",
-    //devtool:"inline-source-map",
-    plugins:[
-        //must not have devtool on options, and mode must be development
-        new (require("webpack").SourceMapDevToolPlugin)({
-            filename:'../dist/cloud.js.map',
-            module:false,
-            append: `\n//# sourceMappingURL=http://localhost:${require("./package.json").config.devPort}/cloud.js.map`
-        })
-    ],
+    //devtool:"eval-source-map",
     module: {
         rules: [
             {
