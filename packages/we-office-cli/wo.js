f768cfca9133f38f7d319d871358fd0ff0640def
lalalic <lalalic@139.com>
2019-11-07 16:07:19 +0800

    fix cli

--- packages/we-office-cli/wo.js
+++ packages/we-office-cli/wo.js
@@ -4,6 +4,7 @@ const chalk=require("chalk")
 const fs=require("fs")
 const path=require("path")
 const merge=require("lodash.merge")
+const { execSync } = require('child_process')
 
 const cwd=process.cwd()
 const {
